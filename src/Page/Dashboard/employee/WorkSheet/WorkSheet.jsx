34e49eed40ebdf81ecdd652838d4e9611dd4d349
abdullah107189 <abdullah107189@gmail.com>
2025-01-15 10:24:04 +0600

    Create update sheet feature where are people update her sheet if can mistack

--- src/Page/Dashboard/employee/WorkSheet/WorkSheet.jsx
+++ src/Page/Dashboard/employee/WorkSheet/WorkSheet.jsx
@@ -11,122 +11,234 @@ import { FaFilePen } from "react-icons/fa6";
 import Swal from "sweetalert2";
 
 const WorkSheet = () => {
-    const axiosPubilc = useAxiosPublic()
-    const { user } = useAuth()
-    const [startDate, setStartDate] = useState(new Date());
-    const { data: workSheetData = [], refetch } = useQuery({
-        queryKey: ['work-sheet', user?.email],
-        queryFn: async () => {
-            const { data: getWorkSheet } = await axiosPubilc.get(`/work-sheet/${user?.email}`)
-            return getWorkSheet;
-        }
-    })
+  const axiosPubilc = useAxiosPublic();
+  const [showModal, setShowModal] = useState({ isOpen: false, sheet: "" });
+  const { user } = useAuth();
+  const [startDate, setStartDate] = useState(new Date());
+  const { data: workSheetData = [], refetch } = useQuery({
+    queryKey: ["work-sheet", user?.email],
+    queryFn: async () => {
+      const { data: getWorkSheet } = await axiosPubilc.get(
+        `/work-sheet/${user?.email}`
+      );
+      return getWorkSheet;
+    },
+  });
 
-    const handleSubmitSheet = async (e) => {
-        e.preventDefault()
-        const form = e.target;
-        const workPosition = form.workPosition.value
-        const hours = form.hours.value;
-        if (workPosition === 'Your Position') {
-            return toast.error('please select work position')
-        }
-        // send data to db
-        const sheetData = {
-            work: workPosition,
-            hours: hours,
-            date: startDate,
-            email: user?.email
-        }
-        const { data } = await axiosPubilc.post('/work-sheet', sheetData)
-        if (data.insertedId) {
-            toast.success('Work sheet Added 👍')
-        }
-        e.target.reset()
-        setStartDate(new Date())
-        refetch()
+  const handleSubmitSheet = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const workPosition = form.workPosition.value;
+    const hours = form.hours.value;
+    if (workPosition === "Your Position") {
+      return toast.error("please select work position");
     }
-    const handleDeletSheet = async (id) => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                const { data: sheetDeleteInfo } = await axiosPubilc.delete(`/work-sheet/${id}`)
-                if (sheetDeleteInfo.deletedCount) {
-                    toast.success('sheet delete successful ')
-                    refetch()
-                }
-
-            }
-        });
-
-
+    // send data to db
+    const sheetData = {
+      work: workPosition,
+      hours: hours,
+      date: startDate,
+      email: user?.email,
+    };
+    const { data } = await axiosPubilc.post("/work-sheet", sheetData);
+    if (data.insertedId) {
+      toast.success("Work sheet Added 👍");
     }
-    return (
-        <div>
-            <h1 className="text-3xl text-center">Employee Work Sheet</h1>
-            <form className="mt-2 flex" onSubmit={handleSubmitSheet}>
+    e.target.reset();
+    setStartDate(new Date());
+    refetch();
+  };
+
+  // sheet delete
+  const handleDeletSheet = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const { data: sheetDeleteInfo } = await axiosPubilc.delete(
+          `/work-sheet/${id}`
+        );
+        if (sheetDeleteInfo.deletedCount) {
+          toast.success("sheet delete successful ");
+          refetch();
+        }
+      }
+    });
+  };
+
+  // sheet update
+  const handleUpdateSheet = async (e, id) => {
+    e.preventDefault();
+    const form = e.target;
+    const workPosition = form.workPosition.value;
+    const hours = form.hours.value;
+    if (workPosition === "Your Position") {
+      return toast.error("please select work position");
+    }
+    // send data to db
+    const sheetData = {
+      work: workPosition,
+      hours: hours,
+      date: startDate,
+    };
+    const { data } = await axiosPubilc.patch(`/work-sheet/update/${id}`, {
+      updateSheet: sheetData,
+    });
+    if (data.matchedCount) {
+      toast.success("Work sheet Updated ✔");
+      refetch();
+      setShowModal({ isOpen: false });
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center">Employee Work Sheet</h1>
+      <form className="mt-2 flex" onSubmit={handleSubmitSheet}>
+        {/* dropdown */}
+        <select
+          required
+          className="p-2 px-4 mr-2 outline-none rounded-full cursor-pointer"
+          name="workPosition"
+          id=""
+        >
+          <option defaultValue={"Your Position"}>Your Position</option>
+          <option value="Frontend Developer">Frontend Developer</option>
+          <option value="Backend Developer">Backend Developer</option>
+          <option value="Fullstack Developer">Fullstack Developer</option>
+          <option value="Software Engineer">Software Engineer</option>
+        </select>
+
+        {/* hours */}
+        <input
+          required
+          className="p-2 px-4 mr-2 outline-none rounded-full cursor-pointer"
+          placeholder="enter hours number"
+          type="number"
+          name="hours"
+          id=""
+        />
+
+        {/* date */}
+        <DatePicker
+          required
+          className="p-2 px-4 outline-none rounded-full cursor-pointer"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+
+        <button className="actionBtn !p-2 !px-5">Submit Sheet</button>
+      </form>
+
+      {/* table data  */}
+      <div className="overflow-x-auto mt-5">
+        <table className="table border">
+          <thead className="">
+            <tr className="pBg text-white">
+              <th className="rounded-tl-lg"></th>
+              <th className="">Task</th>
+              <th className="">Hours Worked</th>
+              <th className="">Date</th>
+              <th className="">Delete</th>
+              <th className="rounded-tr-lg">Update</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {workSheetData?.map((sheet, idx) => (
+              <tr key={sheet._id} className="hover:bg-blue-100 ">
+                <th className="border">{idx + 1}</th>
+                <td className="border">{sheet?.work}</td>
+                <td className="border">{sheet?.hours}</td>
+                <td className="border">{format(sheet?.date, "MMMM yyyy")}</td>
+
+                <td className="border ">
+                  <button onClick={() => handleDeletSheet(sheet?._id)}>
+                    <MdDelete className="w-10 h-10 rounded-full transform duration-300 hover:bg-red-200 p-2 bg-red-100 text-red-400" />
+                  </button>
+                </td>
+                <td className="border ">
+                  <button
+                    className=""
+                    onClick={() =>
+                      setShowModal(
+                        { isOpen: true, sheet: sheet },
+                        setStartDate(sheet.date)
+                      )
+                    }
+                  >
+                    <FaFilePen className="w-10 h-10 rounded-full transform duration-300 hover:bg-red-200 p-2 bg-red-100 text-red-400" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {showModal.isOpen && (
+        <dialog id="my_modal_2" className="modal bg-black/40 " open>
+          <div className="modal-box text-center">
+            <a href="/" className="text-5xl font-bold">
+              <span className="pText">As</span>Tech
+            </a>
+            <div className="min-h-[50vh] flex items-center  justify-center">
+              <form
+                className="mt-2 p-5 grid grid-cols-1 w-full gap-5 "
+                onSubmit={(e) => handleUpdateSheet(e, showModal.sheet._id)}
+              >
                 {/* dropdown */}
-                <select required className="p-2 px-4 mr-2 outline-none rounded-full cursor-pointer" name="workPosition" id="">
-                    <option defaultValue={'Your Position'}>Your Position</option>
-                    <option value="Frontend Developer">Frontend Developer</option>
-                    <option value="Backend Developer">Backend Developer</option>
-                    <option value="Fullstack Developer">Fullstack Developer</option>
-                    <option value="Software Engineer">Software Engineer</option>
+                <select
+                  required
+                  className="p-2 px-4 mr-2 outline-none rounded-full cursor-pointer border"
+                  name="workPosition"
+                  defaultValue={showModal.sheet.work}
+                  id=""
+                >
+                  <option defaultValue={"Your Position"}>Your Position</option>
+                  <option value="Frontend Developer">Frontend Developer</option>
+                  <option value="Backend Developer">Backend Developer</option>
+                  <option value="Fullstack Developer">
+                    Fullstack Developer
+                  </option>
+                  <option value="Software Engineer">Software Engineer</option>
                 </select>
 
                 {/* hours */}
-                <input required className="p-2 px-4 mr-2 outline-none rounded-full cursor-pointer" placeholder="enter hours number" type="number" name="hours" id="" />
+                <input
+                  required
+                  defaultValue={showModal.sheet.hours}
+                  className="p-2 border px-4 mr-2 outline-none rounded-full cursor-pointer"
+                  placeholder="enter hours number"
+                  type="number"
+                  name="hours"
+                  id=""
+                />
 
                 {/* date */}
-                <DatePicker required className="p-2 px-4 outline-none rounded-full cursor-pointer" selected={startDate} onChange={(date) => setStartDate(date)} />
+                <DatePicker
+                  required
+                  className="p-2 w-full border px-4 outline-none rounded-full cursor-pointer"
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
 
                 <button className="actionBtn !p-2 !px-5">Submit Sheet</button>
-            </form>
-            <div className="overflow-x-auto mt-5">
-                <table className="table border">
-                    <thead className="">
-                        <tr className="pBg text-white">
-                            <th className="rounded-tl-lg"></th>
-                            <th className="">Task</th>
-                            <th className="">Hours Worked</th>
-                            <th className="">Date</th>
-                            <th className="">Delete</th>
-                            <th className="rounded-tr-lg">Update</th>
-                        </tr>
-                    </thead>
-                    <tbody className="">
-                        {
-                            workSheetData?.map((sheet, idx) => <tr key={sheet._id} className="hover:bg-blue-100 ">
-                                <th className="border">{idx + 1}</th>
-                                <td className="border">{sheet?.work}</td>
-                                <td className="border">{sheet?.hours}</td>
-                                <td className="border">{format(sheet?.date, 'MMMM yyyy')}</td>
-
-                                <td className="border ">
-                                    <button onClick={() => handleDeletSheet(sheet?._id)}>
-                                        <MdDelete className="w-10 h-10 rounded-full transform duration-300 hover:bg-red-200 p-2 bg-red-100 text-red-400" />
-                                    </button>
-                                </td>
-                                <td className="border ">
-                                    <button className="">
-                                        <FaFilePen className="w-10 h-10 rounded-full transform duration-300 hover:bg-red-200 p-2 bg-red-100 text-red-400" />
-                                    </button>
-                                </td>
-                            </tr>
-                            )
-                        }
-
-                    </tbody>
-                </table>
+              </form>
             </div>
-        </div>
-    );
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button onClick={() => setShowModal(false)}>close</button>
+          </form>
+        </dialog>
+      )}
+    </div>
+  );
 };
 
-export default WorkSheet
+export default WorkSheet;
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,75 +1,145 @@
 import { NavLink } from "react-router-dom";
-import './navbar.css'
+import "./navbar.css";
 import useAuth from "../../hooks/useAuth";
 const Navbar = () => {
-    const { user, logoutUser } = useAuth()
-    const li = <div className="space-x-3">
-        <NavLink to="/" className={({ isActive }) => (`${isActive ? "activeActionBtn !bg-[#678ce4] !text-white" : "border !text-white/80 hover:bg-transparent !border-white/80 actionBtn"}`)}>Home</NavLink>
-        {user && <NavLink to="/dashboard" className={({ isActive }) => (`${isActive ? "activeActionBtn !bg-[#678ce4] !text-white" : "border   !text-white/80 hover:bg-transparent !border-white/80 actionBtn"}`)}>DashBoard</NavLink>}
-        <NavLink to="/contact-us" className={({ isActive }) => (`${isActive ? "activeActionBtn !bg-[#678ce4] !text-white" : "border   !text-white/80 hover:bg-transparent !border-white/80 actionBtn"}`)}>Contact Us</NavLink>
+  const { user, logoutUser } = useAuth();
+  const li = (
+    <div className="space-x-3">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          `${
+            isActive
+              ? "activeActionBtn !bg-[#678ce4] !text-white"
+              : "border !text-white/80 hover:bg-transparent !border-white/80 actionBtn"
+          }`
+        }
+      >
+        Home
+      </NavLink>
+      {user && (
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            `${
+              isActive
+                ? "activeActionBtn !bg-[#678ce4] !text-white"
+                : "border   !text-white/80 hover:bg-transparent !border-white/80 actionBtn"
+            }`
+          }
+        >
+          DashBoard
+        </NavLink>
+      )}
+      <NavLink
+        to="/contact-us"
+        className={({ isActive }) =>
+          `${
+            isActive
+              ? "activeActionBtn !bg-[#678ce4] !text-white"
+              : "border   !text-white/80 hover:bg-transparent !border-white/80 actionBtn"
+          }`
+        }
+      >
+        Contact Us
+      </NavLink>
     </div>
-    const handleLogout = () => {
-        logoutUser()
-    }
-    return (
-        <div className="bg-[#142446] sticky top-0 z-50">
-            <div className="navbar py-0 text-white mxw">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            {li}
-                        </ul>
-                    </div>
-                    <a href="/" className="text-2xl font-bold"><span className="pText">As</span>Tech</a>
-                </div>
-                <div className="navbar-center  hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {li}
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    {
-                        user ? <div className="dropdown dropdown-end text-black">
-                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                                <div className="w-10   rounded-full">
-                                    <img
-                                        className="hover:border-2 border-[#678ce4] transform duration-100 rounded-full"
-                                        alt={user?.displayName}
-                                        src={user?.photoURL} />
-                                </div>
-                            </div>
-                            <ul
-                                tabIndex={0}
-                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                                <li onClick={handleLogout}><a>Logout</a></li>
-                            </ul>
-                        </div>
-                            :
-                            <div>
-                                <NavLink to="/login" className={({ isActive }) => (`${isActive ? "activeActionBtn !bg-[#678ce4] !text-white" : "border    !text-white/80 hover:bg-transparent !border-white/80 actionBtn"} mr-3`)}>Login</NavLink>
-                                <NavLink to="/register" className={({ isActive }) => (`${isActive ? "activeActionBtn !bg-[#678ce4] !text-white" : "border !text-white/80 hover:bg-transparent !border-white/80 actionBtn"} actionBtn`)}>Register</NavLink>
-                            </div>
-                    }
-                </div>
+  );
+  const handleLogout = () => {
+    logoutUser();
+  };
+  return (
+    <div className="bg-[#142446] sticky top-0 z-50">
+      <div className="navbar py-0 text-white mxw">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
-        </div >
-    );
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {li}
+            </ul>
+          </div>
+          <a href="/" className="text-2xl font-bold">
+            <span className="pText">As</span>Tech
+          </a>
+        </div>
+        <div className="navbar-center  hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{li}</ul>
+        </div>
+        <div className="navbar-end">
+          {user ? (
+            <div className="dropdown dropdown-end text-black">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10   rounded-full">
+                  <img
+                    className="hover:border-2 border-[#678ce4] transform duration-100 rounded-full"
+                    alt={user?.displayName}
+                    src={user?.photoURL}
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <li onClick={handleLogout}>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div>
+              <NavLink
+                to="/login"
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "activeActionBtn !bg-[#678ce4] !text-white"
+                      : "border    !text-white/80 hover:bg-transparent !border-white/80 actionBtn"
+                  } mr-3`
+                }
+              >
+                Login
+              </NavLink>
+              <NavLink
+                to="/register"
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "activeActionBtn !bg-[#678ce4] !text-white"
+                      : "border !text-white/80 hover:bg-transparent !border-white/80 actionBtn"
+                  } actionBtn`
+                }
+              >
+                Register
+              </NavLink>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
