64ef4239547ad8c9acb34eb96ccd321f48bede45
abdullah107189 <abdullah107189@gmail.com>
2025-02-19 13:38:37 +0600

    add promotion section

---
+++ src/Page/Home/Home/About/About.jsx
@@ -0,0 +1,39 @@
+import aboutPage from "../../../../assets/about-page.png";
+import SectionHeader from "../../../../component/SectionHeader/SectionHeader";
+const About = () => {
+  return (
+    <section className="bg-gray-100 ">
+      <div className="mb-5">
+        <SectionHeader title={"About Us"}></SectionHeader>
+      </div>
+      <div className="mxw mx-auto px-4">
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="md:w-1/2 mb-5 md:mb-0">
+            <p className="text-gray-600 mb-4">
+              We are a leading company in employee management solutions,
+              dedicated to helping organizations streamline their HR processes
+              and enhance employee engagement. Our mission is to create a better
+              workplace for everyone.
+            </p>
+            <p className="text-gray-600 mb-4">
+              With a team of experienced professionals, we provide innovative
+              tools and strategies that empower businesses to manage their
+              workforce effectively. Our commitment to excellence drives us to
+              continuously improve and adapt to the changing needs of the
+              industry.
+            </p>
+          </div>
+          <div className="md:w-1/2">
+            <img
+              src={aboutPage} // Replace with your image URL
+              alt="About Us"
+              className="w-full h-auto rounded-lg "
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/Page/Home/Home/Home.jsx
+++ src/Page/Home/Home/Home.jsx
@@ -4,19 +4,31 @@ import Slider from "./Slider/Slider";
 import JobPostings from "./JobPostings/JobPostings";
 import FAQSection from "./FAQSection/FAQSection";
 import TeamSection from "./TeamSection/TeamSection";
+import About from "./About/About";
+import Promotion from "./Promotion/Promotion";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+
+      <div className="md:pt-20 pt-10" id="feedback">
+        <About></About>
+      </div>
       <div className="md:pt-20 pt-10" id="services">
         <Services></Services>
       </div>
+
       <div className="md:pt-20 pt-10" id="feedback">
         <Slider></Slider>
       </div>
+
       <div className="md:pt-20 pt-10" id="job_posting">
         <JobPostings></JobPostings>
+      </div>
+
+      <div className="md:pt-20 pt-10" id="job_posting">
+        <Promotion></Promotion>
       </div>
       <div className="md:pt-20 pt-10" id="faq">
         <FAQSection></FAQSection>
